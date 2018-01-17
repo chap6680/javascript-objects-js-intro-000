042b4aae28649cd523b397b4572389b127d8db2b
David Chapman <chap6680misc@gmail.com>
2018-01-17 03:38:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = new Object;
+<<<<<<< HEAD
 playlist ={artist:'Madonna', song:'musical'};
 
 
@@ -10,3 +11,6 @@ function updatePlaylist(playlist,artistName, songTitle) {
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
 }
+=======
+playlist ={artistName:'Madonna', songTitle:'musical'};
+>>>>>>> b754ccf3be4a5317741e6426a42beea469356858
